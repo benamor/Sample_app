2eb0e2cb328852be0df5c5d3f2985ded599771d8
BenAmor <benamor@ubuntu.ubuntu-domain>
2012-03-09 18:18:37 +0100

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
-
+def logo 
+    logo = image_tag("logo.png", alt: "Sample App", class: "round")
+end
 def full_title(page_title)
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
